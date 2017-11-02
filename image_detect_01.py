415f82f47659abe804877dcf61294cb5bfe3ca8b
wangmingfeng1 <wangmingfeng1@wanda.cn>
2017-11-02 17:29:58 +0800

    :tada: 项目初始化

---
+++ image_detect_01.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+def matchAB(fileA, fileB):
+
+    imgA = cv2.imread(fileA)
+    imgB = cv2.imread(fileB)
+
+    grayA = cv2.cvtColor(imgA, cv2.COLOR_BGR2GRAY)
+    grayB = cv2.cvtColor(imgB, cv2.COLOR_BGR2GRAY)
+
+    height, width = grayA.shape
+    
+    result_window = np.zeros((height, width), dtype=imgA.dtype)
+    for start_y in range(0, height-100, 50):
+        for start_x in range(0, width-100, 50):
+            window = grayA[start_y:start_y+100, start_x:start_x+100]
+            match = cv2.matchTemplate(grayB, window, cv2.TM_CCOEFF_NORMED)
+            _, _, _, max_loc = cv2.minMaxLoc(match)
+            matched_window = grayB[max_loc[1]:max_loc[1]+100, max_loc[0]:max_loc[0]+100]
+            result = cv2.absdiff(window, matched_window)
+            result_window[start_y:start_y+100, start_x:start_x+100] = result
+
+    _, result_window_bin = cv2.threshold(result_window, 127, 255, cv2.THRESH_BINARY)
+    _, contours, _ = cv2.findContours(result_window_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    imgC = imgA.copy()
+    for contour in contours:
+        min = np.nanmin(contour, 0)
+        max = np.nanmax(contour, 0)
+        loc1 = (min[0][0], min[0][1])
+        loc2 = (max[0][0], max[0][1])
+        cv2.rectangle(imgC, loc1, loc2, 255, 2)
+
+    plt.subplot(1, 3, 1), plt.imshow(cv2.cvtColor(imgA, cv2.COLOR_BGR2RGB)), plt.title('A'), plt.xticks([]), plt.yticks([])
+    plt.subplot(1, 3, 2), plt.imshow(cv2.cvtColor(imgB, cv2.COLOR_BGR2RGB)), plt.title('B'), plt.xticks([]), plt.yticks([])
+    plt.subplot(1, 3, 3), plt.imshow(cv2.cvtColor(imgC, cv2.COLOR_BGR2RGB)), plt.title('Answer'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+if __name__ == '__main__':
+    matchAB('img/image02-0.png','img/image02-1.png')
